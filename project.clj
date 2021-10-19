45c24e2828ac8572829915f2e1edf6f9626f8473
0918nobita <nobita.0918@gmail.com>
2021-10-19 18:30:13 +0900

    clj-http を使い始めた

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/0918nobita/auto-backup-tweets"
   :license {:name "MIT"}
   :main auto-backup-tweets.core
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [
+                 [org.clojure/clojure "1.10.3"]
+                 [clj-http "3.12.3"]]
   :repl-options {:init-ns auto-backup-tweets.core})
--- src/auto_backup_tweets/core.clj
+++ src/auto_backup_tweets/core.clj
@@ -1,7 +1,14 @@
 (ns auto-backup-tweets.core)
 
+(require '[clj-http.client :as client])
+
 (defn hello [x]
   (println (str "Hello, " x "!")))
 
-(defn -main [& args]
+(defn -main []
+  (-> "https://zenn.dev"
+      (client/get)
+      (get :headers)
+      (get :content-type)
+      (println))
   (hello "world"))
