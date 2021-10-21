94d3a6449972468fb2d4a4340bd11b2727b09091
0918nobita <nobita.0918@gmail.com>
2021-10-21 15:34:16 +0900

    :zap: 単一のツイートの取得とユーザータイムラインの取得を並列で行うようにした

--- src/auto_backup_tweets/core.clj
+++ src/auto_backup_tweets/core.clj
@@ -9,15 +9,20 @@
    :throw-entire-message? true})
 
 (defn- fetch-access-token [consumer_key consumer_secret]
+  (println "Fetching access token ...")
   (-> "https://api.twitter.com/oauth2/token"
       (client/post
        (merge
         http-base-config
         {:basic-auth (map url-encode [consumer_key consumer_secret])
          :content-type "application/x-www-form-urlencoded;charset=UTF-8"
-         :form-params {"grant_type" "client_credentials"}}))))
+         :form-params {"grant_type" "client_credentials"}}))
+      (get :body)
+      (parse-string)
+      (get "access_token")))
 
 (defn- fetch-tweet [access-token tweet-id]
+  (println "Fetching specified tweet ...")
   (-> "https://api.twitter.com/1.1/statuses/show.json"
       (client/get
        (merge
@@ -29,23 +34,26 @@
       (get "text")))
 
 (defn- fetch-user-timeline [access-token screen-name]
+  (println "Fetching user timeline ...")
   (let [res-body (-> "https://api.twitter.com/1.1/statuses/user_timeline.json"
                      (client/get
                       (merge
                        http-base-config
                        {:oauth-token access-token
-                        :query-params {"screen_name" screen-name}}))
+                        :query-params {"screen_name" screen-name
+                                       "include_rts" false
+                                       "count" 200}}))
                      (get :body)
                      (parse-string))]
     (map #(get % "text") res-body)))
 
 (defn -main []
-  (let [consumer-key    (env :twitter-consumer-key)
-        consumer-secret (env :twitter-consumer-secret)
-        access-token    (-> (fetch-access-token consumer-key consumer-secret)
-                            (get :body)
-                            (parse-string)
-                            (get "access_token"))]
-    (println (fetch-tweet access-token "1448546361582915587"))
-    (doseq [tweet (fetch-user-timeline access-token "0918nobita")]
-      (println tweet))))
+  (let [consumer-key     (env :twitter-consumer-key)
+        consumer-secret  (env :twitter-consumer-secret)
+        access-token     (fetch-access-token consumer-key consumer-secret)
+        fetched-tweet    (future (fetch-tweet access-token "1448546361582915587"))
+        fetched-timeline (future (fetch-user-timeline access-token "0918nobita"))]
+    (println @fetched-tweet)
+    (doseq [tweet @fetched-timeline]
+      (println tweet))
+    (shutdown-agents)))
